5643db2cc6f7c6a95239ae541064144bb4c25adc
Mehran <mehranbhat2001@gmail.com>
2024-04-11 15:04:35 +0530

    program files made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <>
         <Navbar />
         <Hero />
+        <Programs />
       </>
     </div>
   );
