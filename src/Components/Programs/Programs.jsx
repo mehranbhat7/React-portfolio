920436c1682b828b7990194a5596a4f3a5d8a21b
Mehran <mehranbhat2001@gmail.com>
2024-04-13 12:22:06 +0530

    added programs that i offeer

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -2,7 +2,42 @@ import React from "react";
 import "./Programs.css";
 
 const Programs = () => {
-  return <div>Programs</div>;
+  return (
+    <>
+      <div className="programs">
+        <div className="program">
+          <img
+            src="https://www.mindinventory.com/blog/wp-content/uploads/2021/03/mobile-app-design-fundamentals-the-difference-between-UI-and-UX.webp"
+            alt=""
+          />
+          <div className="caption">
+            <h1>📝</h1>
+            <h4>UI/UX Design</h4>
+          </div>
+        </div>
+        <div className="program">
+          <img
+            src="https://kinsta.com/wp-content/uploads/2023/02/github-pages.jpg"
+            alt=""
+          />
+          <div className="caption">
+            <h1>🛠</h1>
+            <h4>Version control</h4>
+          </div>
+        </div>
+        <div className="program">
+          <img
+            src="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg"
+            alt=""
+          />
+          <div className="caption">
+            <h1>💻</h1>
+            <h4>Web Development</h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Programs;
