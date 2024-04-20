f39fd51ac3cd8a26f3d06872c9125f02361c6ded
Mehran <mehranbhat2001@gmail.com>
2024-04-20 20:40:03 +0530

    responsive wip

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,33 @@
+// import React from "react";
+// import "./Hero.css";
+
+// const Hero = () => {
+//   return (
+//     <div className="hero">
+//       <div className="hero-txt">
+//         <h1>Mehran Muzaffar</h1>
+//         <h3>Front-End Developer</h3>
+//         <p>
+//           Remote front-end developer skilled in HTML, CSS, JavaScript and
+//           ReactJs. Efficient in translating designs into responsive interfaces.
+//           Collaborative team player with strong communication for successful
+//           project outcomes.
+//         </p>
+//         <button className="btn">explore more</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+
 import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-txt">
+      <div className="hero-content">
         <h1>Mehran Muzaffar</h1>
         <h3>Front-End Developer</h3>
         <p>
